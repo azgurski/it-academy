472d06230f42e3b062ee548df0c88222aa1b4b92
azgurski <116376924+azgurski@users.noreply.github.com>
2022-11-14 16:25:05 +0100

    Update and rename HomeWorkOfLesson5ATask4.java to HomeWork5A.java

--- HomeWork5A.java
+++ HomeWork5A.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class HomeWorkOfLesson5ATask4 {
+public class HomeWork5A {
     public static void main(String[] args) {
         int[][] array = new int[5][5];
         Random random = new Random();
