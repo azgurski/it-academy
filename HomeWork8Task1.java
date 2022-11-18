366aa5f2f85bd36a42c680e871550faf024d03d8
azgurski <116376924+azgurski@users.noreply.github.com>
2022-11-18 17:42:27 +0100

    Add files via upload

---
+++ HomeWork8Task1.java
@@ -0,0 +1,75 @@
+public class HomeWork8Task1 {
+    public static void main(String[] args) {
+        Man man = new Man();
+        man.setName("Alex");
+        man.setAge(26);
+        man.setWeight(83);
+        man.info();
+
+        Student student = new Student();
+        student.setYearStartOfStudying(1990);
+        student.calculateEndOfStudying(4);
+    }
+}
+
+class Man {
+    private String name;
+    private int age;
+
+    private String sex;
+    private int weight;
+
+    public void info() {
+        System.out.print("Name: " + getName());
+        System.out.print("\nAge, years: " + getAge());
+        System.out.print("\nSex: " + getSex());
+        System.out.println("\nWeight, kg: " + getWeight());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private String getSex() {
+        String sex = "M";
+        return sex;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
+
+class Student {
+    private int year;
+    private int duration;
+
+    public void calculateEndOfStudying(int duration) {
+        System.out.println("\nStart of studying: " + getYearStartOfStudying());
+        System.out.println("End of studying: " + (getYearStartOfStudying() + duration));
+    }
+
+    public int getYearStartOfStudying() {
+        return year;
+    }
+
+    public void setYearStartOfStudying(int year) {
+        this.year = year;
+    }
+}
