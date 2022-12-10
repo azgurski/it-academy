008d01229dfe5b82b577ba49b22a6a18ee1e5100
azgurski <116376924+azgurski@users.noreply.github.com>
2022-12-10 14:30:36 +0100

    Add files via upload

---
+++ HomeWork11Task1.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class HomeWork11Task1 {
+    public static void main(String[] args) {
+        Scanner scanner;
+        Random random = new Random();
+        int guessedNumber = random.nextInt(1, 100);
+        int userNumber;
+        System.out.println("The computer chose a number between 1 and 100. Try to guess it:");
+
+        while (true) {
+            try {
+                scanner = new Scanner(System.in);
+                userNumber = scanner.nextInt();
+
+                if (userNumber > 100 || userNumber < 1) {
+                    System.out.println("The computer doesn't play like that. Your number is not between 1 and 100. Try again.");
+
+                } else if (userNumber < guessedNumber) {
+                    System.out.println("Guessed number is more. Try again.");
+
+                } else if (userNumber > guessedNumber) {
+                    System.out.println("Guessed number is less. Try again.");
+
+                } else {
+                    System.out.println("\nYou won! The secret number is " + guessedNumber + ".");
+                    scanner.close();
+                    break;
+                }
+
+            } catch (InputMismatchException ex) {
+                System.out.println("It doesn't look like a number to guess... Try again.");
+            }
+        }
+    }
+}
+
